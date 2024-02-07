9acb810827dbe60259096f032ae73879847eb0f4
rocandpar <randujarpareja@gmail.com>
2024-02-07 12:44:07 +0100

    Create practicaGestionDatosR.R

---
+++ practicaGestionDatosR.R
@@ -0,0 +1,13 @@
+#1
+edades_descubrimientos <- c(1981,1932,1940,2011,1996,1984,1905,1956,2023,1974,1975,1898,1922,2002)
+mean(edades_descubrimientos)
+print(edades_descubrimientos)
+
+#2
+cantidad_artefactos <- c(48,2000,405,765,983,199,56)
+valor_suma <- 48+2000+405+765+983+199+56
+print(valor_suma)
+
+#3
+profundidad_hallazgos <- c(1,2,3,4,5,6)
+
